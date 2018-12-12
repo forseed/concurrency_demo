95b3684ca5e064ce4ea196dd658d78ac2ec6fbfe
forseed <forgame@aliyun.com>
2018-12-12 21:36:34 +0800

    模板方法

--- src/main/java/patterns/template_pattern/MyThread.java
+++ src/main/java/patterns/template_pattern/MyThread.java
@@ -5,7 +5,7 @@ public abstract class MyThread {
     abstract void run();
 
     public final void start() {
-        System.out.println("调用前处理。。。。");
+        System.out.println("调用前处理。。。。。");
         run();
         System.out.println("调用后处理。。。。");
     }
