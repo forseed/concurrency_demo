3116047f6bcb6bad71b58a48cc1472b5f9f9eb6d
forseed <forgame@aliyun.com>
2018-12-30 18:28:57 +0800

    future

--- src/main/java/patterns/future_pattern/Host.java
+++ src/main/java/patterns/future_pattern/Host.java
@@ -1,12 +1,15 @@
 package patterns.future_pattern;
 
+import java.util.function.Consumer;
+
 public class Host {
-    public Data request(int count, char c) {
+    public Data request(int count, char c, Consumer<String> consumer) {
         System.out.println("request count:" + count + " c:" + c);
         final FutureData future = new FutureData();
         new Thread(() -> {
             RealData real = new RealData(count, c);
             future.setRealData(real);
+            consumer.accept(real.getContent());
         }).start();
         System.out.println("request count:" + count + " c:" + c + " end");
         return future;
--- src/main/java/patterns/future_pattern/Main.java
+++ src/main/java/patterns/future_pattern/Main.java
@@ -3,15 +3,15 @@ package patterns.future_pattern;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Host host = new Host();
-        Data data1 = host.request(10, 'A');
-        Data data2 = host.request(20, 'B');
-        Data data3 = host.request(30, 'C');
+        Data data1 = host.request(10, 'A', System.out::println);
+        Data data2 = host.request(20, 'B', System.out::println);
+        Data data3 = host.request(30, 'C', System.out::println);
 
 
 //        Thread.sleep(5000);
 
-        System.out.println(data1.getContent());
-        System.out.println(data2.getContent());
-        System.out.println(data3.getContent());
+//        System.out.println(data1.getContent());
+//        System.out.println(data2.getContent());
+//        System.out.println(data3.getContent());
     }
 }
