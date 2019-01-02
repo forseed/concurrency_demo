354efed16dd4907b15bb4e278017399b3b428a26
forseed <forgame@aliyun.com>
2019-01-02 10:14:08 +0800

    CASlock

--- src/main/java/demo/lock/caslock/CASLock.java
+++ src/main/java/demo/lock/caslock/CASLock.java
@@ -5,11 +5,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CASLock {
     final AtomicInteger atomicInteger = new AtomicInteger(0);
 
+    private Thread current;
+
     public void lock() throws LockException {
         boolean res = atomicInteger.compareAndSet(0, 1);
         if (!res) {
             throw new LockException("has lock..");
         }
+
+        current = Thread.currentThread();
     }
 
     public void unlock() {
@@ -17,6 +21,8 @@ public class CASLock {
             return;
         }
 
-        atomicInteger.compareAndSet(1, 0);
+        if (current == Thread.currentThread()) {
+            atomicInteger.compareAndSet(1, 0);
+        }
     }
 }
--- src/main/java/demo/lock/caslock/Main.java
+++ src/main/java/demo/lock/caslock/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 5; i++) {
             new Thread(() -> {
                 try {
                     doSomething();
